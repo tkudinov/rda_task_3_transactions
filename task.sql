8cf9d847baa3a40f7ca275ab751ea1abf4c7a3d0
Taras Kudinov <38283923+tkudinov@users.noreply.github.com>
2025-02-10 12:27:33 +0200

    Added required marks into task.sql.

--- task.sql
+++ task.sql
@@ -2,24 +2,21 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
-ALTER TABLE Orders
-ADD Count INT;
-
-INSERT INTO Products (ID, Name, WarehouseAmount)
-VALUES ("1", "AwersomeProduct", "1");
+INSERT INTO Products (ID, Name, Description, Price, WarehouseAmount)
+VALUES ('1', 'AwersomeProduct', 'Its good.', '15', '1');
 
 INSERT INTO Customers (ID, FirstName, LastName, Email, Address)
-VALUES ("1", "Paul", "Cole", "pk@net.com", "Rome");
+VALUES ('1', 'Paul', 'Cole', 'pk@net.com', 'Rome');
 
 INSERT INTO Orders (ID, CustomerID, Date )
-VALUES ("1", "1", "2025-02-10");
+VALUES ('1', '1', '2025-02-10');
 
-INSERT INTO OrderItems (ID, OrderID, ProductID)
-VALUES ("1", "1", "1");
+INSERT INTO OrderItems (ID, OrderID, ProductID, Count)
+VALUES ('1', '1', '1', '1');
 -- Start the transaction 
 START TRANSACTION; 
 
 -- And some data should be created inside the transaction 
-UPDATE Orders SET Count = 1;
-UPDATE Orders SET Count = Count - 1 WHERE ID = '1';
+UPDATE Products SET WarehouseAmount = WarehouseAmount - OrderItems.Count WHERE OrderID = '1';
+
 COMMIT; 
